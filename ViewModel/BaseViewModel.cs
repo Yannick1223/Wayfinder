9062c3f529a377b2275475a896a6e029b44b2ccb
Yannick <53968456+Yannick1223@users.noreply.github.com>
2025-01-02 18:26:26 +0100

    Projektdateien hinzufügen.

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Wayfinder.ViewModel
+{
+    public partial class BaseViewModel: ObservableObject
+    {
+        public BaseViewModel()
+        {
+            Title = "";
+        }
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        bool isBusy;
+        [ObservableProperty]
+        string title;
+
+        public bool IsNotBusy => !IsBusy;
+    }
+}
