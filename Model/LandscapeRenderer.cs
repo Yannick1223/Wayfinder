8a1661a65d50e569851dfbf961866e3f5f8ae801
Yannick <53968456+Yannick1223@users.noreply.github.com>
2025-01-06 16:12:28 +0100

    implemented LandscapeRenderer

---
+++ Model/LandscapeRenderer.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Net.NetworkInformation;
+
+namespace Wayfinder.Model
+{
+    public class LandscapeRenderer
+    {
+        public LandscapeRenderer(int _row, int _column)
+        {
+            LandscapeRow = _row;
+            LandscapeColumn = _column;
+
+            //Default Settings
+            LandscapeTileWidth = 32;
+            LandscapeTileHeight = 32;
+
+            OutlineThickness = 2;
+
+            CalculateImageSize();
+
+            Landscape = CreateLandscapeBitmap(ImageWidth, ImageHeight);
+        }
+
+        public WriteableBitmap Landscape { get; private set; }
+        //Overall size of the image = CalculateImageSize();
+        public int ImageWidth { get; private set; }
+        public int ImageHeight { get; private set; }
+
+        //Size of the individual tiles (32px*32px)
+        public int LandscapeTileWidth { get; private set; }
+        public int LandscapeTileHeight { get; private set; }
+
+        //Number of tiles
+        public int LandscapeRow { get; private set; }
+        public int LandscapeColumn { get; private set; }
+
+        public int OutlineThickness { get; private set; }
+
+        private WriteableBitmap CreateLandscapeBitmap(int _width, int _height)
+        {
+            return BitmapFactory.New(_width, _height);
+        }
+
+        public void SetLandscapeSize(int _row, int _column)
+        {
+            LandscapeRow = _row;
+            LandscapeColumn = _column;
+
+            CalculateImageSize();
+        }
+
+        public void SetTileSize(int _width, int _height)
+        {
+            if(_width < 0 || _height < 0)
+            {
+                throw new ArgumentException("Argument can't be below zero. Width:" +  _width + " Height: " + _height); //own exeption later
+            }
+
+            LandscapeTileWidth = _width;
+            LandscapeTileHeight = _height;
+
+            CalculateImageSize();
+        }
+
+        public void SetOutlineThickness(int _thickness)
+        {
+            OutlineThickness = _thickness;
+
+            CalculateImageSize();
+        }
+
+        public void CalculateImageSize()
+        {
+            //All Tile Pixel + Border/Edge + between + Tile Pixel Border
+            ImageWidth = LandscapeRow * LandscapeTileWidth + (LandscapeRow - 1) * OutlineThickness + 2 * OutlineThickness;
+            ImageHeight = LandscapeColumn * LandscapeTileHeight + (LandscapeColumn - 1) * OutlineThickness + 2 * OutlineThickness;
+        }
+
+
+        public void DrawPixel(int _x, int _y, Color _color)
+        {
+            using (Landscape.GetBitmapContext())
+            {
+                Landscape.SetPixel(_x, _y, _color);
+            }
+        }
+
+        public void DrawLine(int _x1, int _y1, int _x2, int _y2, Color _color)
+        {
+            using (Landscape.GetBitmapContext())
+            {
+                Landscape.DrawLine(_x1, _y1, _x2, _y2, _color);
+            }
+        }
+
+        public void DrawHorizontalLine(int _x1, int _x2, int _y1, Color _color)
+        {
+            DrawLine(_x1, _y1, _x2, _y1, _color);
+        }
+
+        public void DrawVerticalLine(int _y1, int _y2, int _x1, Color _color)
+        {
+
+            DrawLine(_x1, _y1, _x1, _y2, _color);
+        }
+
+        public void DrawRectangle(int _x1, int _y1, int _x2, int _y2, Color _color)
+        {
+            using (Landscape.GetBitmapContext())
+            {
+                Landscape.DrawRectangle(_x1, _y1, _x2, _y2, _color);
+            }
+        }
+
+        public void DrawFillRectangle(int _x1, int _y1, int _x2, int _y2, Color _color)
+        {
+            using (Landscape.GetBitmapContext())
+            {
+                Landscape.FillRectangle(_x1, _y1, _x2, _y2, _color);
+            }
+        }
+
+        public void DrawFillRectangle(Point from, Point to, Color _color)
+        {
+            DrawFillRectangle((int)from.X, (int)from.Y, (int)to.X, (int)to.Y, _color);
+        }
+
+        public void DrawLandscapeOutline()
+        {
+            if(OutlineThickness > 0)
+            {
+                DrawBorder();
+                DrawHorizontalOutlines();
+                DrawVertícalOutlines();
+            }
+        }
+
+        private void DrawBorder()
+        {
+
+            for(int thickness = 0; thickness < OutlineThickness; thickness++)
+            {
+                DrawRectangle(thickness, thickness, ImageWidth - thickness - 1, ImageHeight - thickness - 1, Colors.Black);
+            }
+        }
+
+        private void DrawHorizontalOutlines()
+        {
+            using (Landscape.GetBitmapContext())
+            {
+                for(int y = 1; y < LandscapeColumn; y++)
+                {
+                    Point startPosition = new Point(0, y * LandscapeTileWidth + y * OutlineThickness);
+                    Point endPosition = new Point(ImageWidth, y * LandscapeTileWidth + y * OutlineThickness + OutlineThickness);
+
+                    DrawFillRectangle(startPosition, endPosition, Colors.Black);
+                }
+            }
+        }
+
+        private void DrawVertícalOutlines()
+        {
+            using (Landscape.GetBitmapContext())
+            {
+                for (int x = 1; x < LandscapeRow; x++)
+                {
+                    Point startPosition = new Point(x * LandscapeTileHeight + x * OutlineThickness, 0);
+                    Point endPosition = new Point(x * LandscapeTileWidth + x * OutlineThickness + OutlineThickness, ImageWidth);
+
+                    DrawFillRectangle(startPosition, endPosition, Colors.Black);
+                }
+            }
+        }
+
+        public void DrawColorTile(int _row, int _col, Color _color)
+        {
+            if(_row <= 0 || _col <= 0)
+            {
+                throw new ArgumentException("row or col can't be below 1");
+            }
+
+            using (Landscape.GetBitmapContext())
+            {
+                Point startTile = new Point(_row * LandscapeTileWidth + (_row - 1) * OutlineThickness + OutlineThickness - LandscapeTileWidth, _col * LandscapeTileHeight + (_col - 1) * OutlineThickness + OutlineThickness - LandscapeTileHeight);
+                Point endTile = new Point(startTile.X + LandscapeTileWidth, startTile.Y + LandscapeTileHeight);
+
+                DrawFillRectangle(startTile, endTile, _color);
+            }
+        }
+
+        public void DrawImageAtTile(int _row, int _col, WriteableBitmap _image)
+        {
+            if (_row <= 0 || _col <= 0)
+            {
+                throw new ArgumentException("row or col can't be below 1");
+            }
+
+            using (Landscape.GetBitmapContext())
+            {
+                //Point startTile = new Point(_row * LandscapeTileWidth + (_row - 1) * OutlineThickness + OutlineThickness - LandscapeTileWidth, _col * LandscapeTileHeight + (_col - 1) * OutlineThickness + OutlineThickness - LandscapeTileHeight);
+                //Point endTile = new Point(startTile.X + LandscapeTileWidth, startTile.Y + LandscapeTileHeight);
+
+                //Point startTile = new Point(2, 2);
+
+                //Landscape.Blit(startTile, _image, new Rect(0, 0, 32, 32), Colors.White, WriteableBitmapExtensions.BlendMode.None);
+            }
+        }
+
+        public void DrawImageTileAtPosition(int _x1, int _y1, WriteableBitmap _image)
+        {
+            using (Landscape.GetBitmapContext())
+            {
+                Landscape.Blit(new Point(_x1, _y1), _image, new Rect(_x1, _y1, _image.Width, _image.Height), Color.FromArgb(255, 255, 255, 255), WriteableBitmapExtensions.BlendMode.None);
+            }
+        }
+    }
+}
--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -6,9 +6,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wayfinder.ViewModel;
+using Wayfinder.Model; //TODO: in ViewModel integrieren
 
 namespace Wayfinder
 {
@@ -21,6 +23,83 @@ namespace Wayfinder
         {
             DataContext = new BaseViewModel();
             InitializeComponent();
+            RenderOptions.SetBitmapScalingMode(Landscape, BitmapScalingMode.NearestNeighbor);
+
+            ImageTests();
+        }
+
+        LandscapeRenderer rend;
+
+
+        public void ImageTests()
+        {
+            rend = new LandscapeRenderer(100, 100);
+            Landscape.Source = rend.Landscape;
+
+            //rend.DrawFillRectangle(0, 0, 32, 32, Colors.Red);
+
+
+
+
+            Random rnd = new Random();
+            /*for (int x = 1; x < rend.ImageWidth; x+=rend.LandscapeTileWidth)
+            {
+                for (int y = 1; y < rend.ImageHeight; y+= rend.LandscapeTileHeight)
+                {
+                    //rend.DrawPixel(x, y, Color.FromRgb((byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255)));
+
+                    rend.DrawFillRectangle(x, y, x + rend.LandscapeTileWidth + rend.OutlineThickness, y + rend.LandscapeTileHeight + rend.OutlineThickness, Color.FromRgb((byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255)));
+                }
+            }*/
+            string path = @"C:\Users\Admin\source\repos\Wayfinder\Assets\test1.png";
+
+            BitmapImage a = new BitmapImage();
+            a.BeginInit();
+            a.UriSource = new Uri(path, UriKind.Absolute);
+            a.EndInit();
+
+            WriteableBitmap test = new WriteableBitmap(a);
+
+
+
+            Console.WriteLine("Test");
+            for (int x = 1; x < rend.LandscapeRow + 1; x++)
+            {
+                for (int y = 1; y < rend.LandscapeColumn + 1; y++)
+                {
+                    rend.DrawColorTile(x, y, Color.FromRgb((byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255)));
+                }
+            }
+            rend.DrawLandscapeOutline();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rend.DrawLandscapeOutline();
+            /*Random rnd = new Random();
+            for (int x = 0; x < rend.ImageWidth; x++)
+            {
+                for (int y = 0; y < rend.ImageHeight; y++)
+                {
+                    rend.DrawPixel(x, y, Color.FromRgb((byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255)));
+                }
+            }*/
+        }
+
+        private void Landscape_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point mousePosition = e.GetPosition(Landscape);
+
+            // Skalierungsfaktoren berechnen
+            double scaleX = Landscape.ActualWidth / Landscape.Source.Width;
+            double scaleY = Landscape.ActualHeight / Landscape.Source.Height;
+
+            // Umrechnen der Mausposition in Bildkoordinaten
+            int imageX = (int)(mousePosition.X / scaleX) + 1;
+            int imageY = (int)(mousePosition.Y / scaleY) + 1;
+
+            // Ausgabe der Bildkoordinaten
+            MessageBox.Show($"Die angeklickte Pixelposition im Bild ist: X={imageX}, Y={imageY}");
         }
     }
 }
